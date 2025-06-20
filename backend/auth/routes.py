8f64d907561a02839f92631fccea564ddff29ef6
Adil Zahid <adilzahid26@gmail.com>
2025-06-19 20:25:57 -0500

    Make register endpoint in backend

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -23,7 +23,7 @@ def login():
 
         conn = get_db_connection()
         cursor = conn.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM login WHERE email=%s", (email,))
+        cursor.execute("SELECT * FROM user WHERE email=%s", (email,))
         user = cursor.fetchone()
         cursor.close()
         conn.close()
@@ -34,6 +34,37 @@ def login():
             return jsonify({"message": "Incorrect Password"}), 401
         else:
             return jsonify({"message": "Invalid email"}), 401
+    except Exception as e:
+        print("ERROR:", e)
+        return jsonify({"error": str(e)}), 500
+    
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    try:
+        data = request.json
+        firstName = data.get('first_name')
+        lastName = data.get('last_name')
+        email = data.get('email')
+        password = data.get('password')
+
+        conn = get_db_connection()
+        cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM user WHERE email=%s", (email,))
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if user:
+            return jsonify({"message": "Email already exists"}), 401
+        else: 
+            conn = get_db_connection()
+            cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("INSERT INTO user (first_name, last_name, email, password) VALUES (%s, %s, %s, %s)", (firstName, lastName, email, password))
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+            return jsonify({"message": "User created"})
     except Exception as e:
         print("ERROR:", e)
         return jsonify({"error": str(e)}), 500
